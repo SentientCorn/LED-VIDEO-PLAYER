d8ff96449d930fa5b196f5fe38851191493ddc59
SentientCorn <radityanarotama0@gmail.com>
2024-03-01 12:44:28 +0700

    Makes display a function

--- converter/converter.py
+++ converter/converter.py
@@ -14,6 +14,14 @@ def print_list(my_list):
     for item in my_list:
         print(item)
 
+def display_section (_input, _name, _QWidth, _QHeight, _SWidth, _SHeight, _grid):
+  display = np.copy(_input)
+  if _grid == 1:
+    for i in range (1,8):
+      cv2.line(display, (0, _SHeight * i), (_QWidth, _SHeight * i), (0, 0, 255), 1)
+      cv2.line(display, (_SWidth * i, 0), (_SWidth * i, _QHeight), (0, 0, 255), 1)
+  cv2.imshow(f"{_name}", display)
+
 print("Attempting to read video...")
 cap = cv2.VideoCapture('converter\Bad Apple.mp4')
 
@@ -26,8 +34,6 @@ frame_count = cv2.CAP_PROP_FRAME_COUNT
 fps = cap.get(cv2.CAP_PROP_FPS)
 width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 centerX, centerY = int((width // 2)), int((height // 2))
-
-output = cv2.VideoWriter('output.mp4',cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height))
 
 while (cap.isOpened()):
     read, fullSection = cap.read()  # Full frame of the video
@@ -46,16 +52,16 @@ while (cap.isOpened()):
     Q_SizeX, Q_SizeY        = int(width // 2), int(height // 2)
     S_Width, S_Height       = int(width // 16), int(height // 16)
 
+    # Display sections (Because why not?)
+    display_section(topLeft,"Top Left", Q_SizeX, Q_SizeY, S_Width, S_Height, 1)
+    display_section(topRight,"Top Right", Q_SizeX, Q_SizeY, S_Width, S_Height, 1)
+    display_section(bottomLeft,"Bottom Left", Q_SizeX, Q_SizeY, S_Width, S_Height, 1)
+    display_section(bottomRight,"Bottom Right", Q_SizeX, Q_SizeY, S_Width, S_Height, 1) 
+
     # Convert to 8x8 square
     # Top left
-    gridImage = np.copy(topLeft)  # Create a copy of topLeft to draw grid lines
 
-    # Horizontal Line
-    for i in range(1, 8):
-        cv2.line(gridImage, (0, S_Height * i), (Q_SizeX, S_Height * i), (0, 0, 255), 1)
-    # # Vertical Line
-    # for j in range(1, 8):
-        cv2.line(gridImage, (S_Width * i, 0), (S_Width * i, Q_SizeY), (0, 0, 255), 1)
+
 
     for i in range(0, 8):
         for j in range(0, 8):
@@ -63,8 +69,6 @@ while (cap.isOpened()):
             TLarray.append(light_dark(roi))
             # print(light_dark(roi), "\t", i + 1, "x", j + 1)
     
-    output.write(gridImage)  # Write gridImage instead of topLeft
-    cv2.imshow("Bad Apple", gridImage)
     if cv2.waitKey(1) & 0xFF == ord('s'):
         break
 
@@ -72,8 +76,7 @@ while (cap.isOpened()):
 
 
 
-print_list(TLarray)
+# print_list(TLarray)
 
 cap.release()
-output.release()
 cv2.destroyAllWindows()
